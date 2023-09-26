843fdd5a716196f9e561f42fdd3bf9c000bbc200
henriquebenjamim <henriiqqq@gmail.com>
2023-09-26 01:01:17 -0300

    Feat: tests methods but one is not working, i need to fix success test on create Todo

--- todolist-java/src/test/java/br/com/henriquebenjamim/todolistjava/TodolistJavaApplicationTests.java
+++ todolist-java/src/test/java/br/com/henriquebenjamim/todolistjava/TodolistJavaApplicationTests.java
@@ -1,13 +1,49 @@
 package br.com.henriquebenjamim.todolistjava;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.description;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.com.henriquebenjamim.todolistjava.entities.Todo;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodolistJavaApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void shouldCreateTodoWithSuccess() {
+		var todo = new Todo("todo 1", "desc todo 1", false, 1);
+		
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].name").isEqualTo(todo.getName())
+			.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+			.jsonPath("$[0].isDone").isEqualTo(todo.isDone())
+			.jsonPath("$[0].priority").isEqualTo(todo.getPriority());
 	}
 
+	@Test
+	void shouldFailOnCreateTodo() {
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(
+				new Todo("", "", false, 0)
+			)
+			.exchange()
+			.expectStatus().isBadRequest();
+	}
 }
